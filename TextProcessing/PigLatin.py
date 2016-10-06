0577b7fdeb8f8f2c0b0233e910dd7a88b23fb6a4
Choumingzhao <choumingzhao@outlook.com>
2016-10-06 15:18:14 +0800

    Update PigLatin.py

--- TextProcessing/PigLatin.py
+++ TextProcessing/PigLatin.py
@@ -1,1 +1,22 @@
+# -*- encoding:utf-8 -*-
+import re
 
+
+def pigLatin(word):
+    if word.isalpha() is False:
+        raise TypeError
+    lower = word.lower()
+    # When word starts with vowel
+    if all([not lower.startswith(i) for i in 'aeiou']):
+        head = re.match(r'[bcdfghjklmnpqrstvwxyz]+', lower).group()
+        result = lower.replace(head, '')+head+'ay'
+    else:
+        result = word+"way"
+    if word[0].isupper():
+        return result.capitalize()
+    else:
+        return result
+
+if __name__ == "__main__":
+    word = "Hello"
+    print(pigLatin(word))
